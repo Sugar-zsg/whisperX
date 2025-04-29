93e6f2de102277622e366e4d87032a1523b8b80b
Sugar-zsg <64777228+Sugar-zsg@users.noreply.github.com>
2025-04-29 17:01:26 +0800

    Update alignment.py

--- whisperx/alignment.py
+++ whisperx/alignment.py
@@ -114,8 +114,7 @@ def align(
 ) -> AlignedTranscriptionResult:
     """
     Align phoneme recognition predictions to known transcription.
-    """
-    begin_time = time.time()    
+    """   
     if not torch.is_tensor(audio):
         if isinstance(audio, str):
             audio = load_audio(audio)
@@ -225,7 +224,8 @@ def align(
                 waveform_segment, (0, 400 - waveform_segment.shape[-1])
             )
         else:
-            lengths = None     
+            lengths = None
+        
         waveform_segment = waveform_segment.to(torch.float16)
         with torch.inference_mode():
             if model_type == "torchaudio":
@@ -237,7 +237,6 @@ def align(
             emissions = torch.log_softmax(emissions, dim=-1)
 
         emission = emissions[0].cpu().detach()
-
 
         blank_id = 0
         for char, code in model_dictionary.items():
@@ -253,7 +252,6 @@ def align(
             continue
 
         char_segments = merge_repeats(path, text_clean)
-
 
         duration = t2 -t1
         ratio = duration * waveform_segment.size(0) / (trellis.size(0) - 1)
@@ -401,13 +399,72 @@ def get_trellis(emission, tokens, blank_id=0):
         trellis[t+1, 1:] = torch.maximum(stay_scores, move_scores)
 
     return trellis
+'''
+def get_trellis(emission, tokens, blank_id=0):
+    num_frame = emission.size(0)
+    num_tokens = len(tokens)
+
+    # Trellis has extra diemsions for both time axis and tokens.
+    # The extra dim for tokens represents <SoS> (start-of-sentence)
+    # The extra dim for time axis is for simplification of the code.
+    trellis = torch.empty((num_frame + 1, num_tokens + 1))
+    trellis[0, 0] = 0
+    trellis[1:, 0] = torch.cumsum(emission[:, 0], 0)
+    trellis[0, -num_tokens:] = -float("inf")
+    trellis[-num_tokens:, 0] = float("inf")
+
+    for t in range(num_frame):
+        trellis[t + 1, 1:] = torch.maximum(
+            # Score for staying at the same token
+            trellis[t, 1:] + emission[t, blank_id],
+            # Score for changing to the next token
+            trellis[t, :-1] + emission[t, tokens],
+        )
+    return trellis
+'''
 
 @dataclass
 class Point:
     token_index: int
     time_index: int
     score: float
+'''
+def backtrack(trellis, emission, tokens, blank_id=0):
+    # Note:
+    # j and t are indices for trellis, which has extra dimensions
+    # for time and tokens at the beginning.
+    # When referring to time frame index `T` in trellis,
+    # the corresponding index in emission is `T-1`.
+    # Similarly, when referring to token index `J` in trellis,
+    # the corresponding index in transcript is `J-1`.
+    j = trellis.size(1) - 1
+    t_start = torch.argmax(trellis[:, j]).item()
 
+    path = []
+    for t in range(t_start, 0, -1):
+        # 1. Figure out if the current position was stay or change
+        # Note (again):
+        # `emission[J-1]` is the emission at time frame `J` of trellis dimension.
+        # Score for token staying the same from time frame J-1 to T.
+        stayed = trellis[t - 1, j] + emission[t - 1, blank_id]
+        # Score for token changing from C-1 at T-1 to J at T.
+        changed = trellis[t - 1, j - 1] + emission[t - 1, tokens[j - 1]]
+
+        # 2. Store the path with frame-wise probability.
+        prob = emission[t - 1, tokens[j - 1] if changed > stayed else 0].exp().item()
+        # Return token index and time index in non-trellis coordinate.
+        path.append(Point(j - 1, t - 1, prob))
+
+        # 3. Update the token
+        if changed > stayed:
+            j -= 1
+            if j == 0:
+                break
+    else:
+        # failed
+        return None
+    return path[::-1]
+'''
 def backtrack(trellis, emission, tokens, blank_id=0):
     # Convert tokens to a list for faster access if it's a tensor
     if isinstance(tokens, torch.Tensor):
